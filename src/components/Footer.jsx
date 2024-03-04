2d58c13f06c66e78bcf6d57030c5a89a56d86cc3
akshayviola <akshaysunil201@gmail.com>
2024-03-04 11:11:47 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import InstagramIcon from '@mui/icons-material/Instagram';
-
+import EmailIcon from '@mui/icons-material/Email';
 
 const Footer = () => {
   return (
@@ -13,6 +13,7 @@ const Footer = () => {
           <Card.Text>
            <div className='foot-div'>
               <InstagramIcon style={{color:'white',width:'70px',height:'70px'}}/>
+              <EmailIcon style={{color:'white',width:'70px',height:'70px',margin:'10px'}}/>
             <p style={{color:'white'}} className='text-center'>All Rights &copy;  <b>FocusFrames</b> &reg; 2024</p>
            </div>
          </Card.Text>
